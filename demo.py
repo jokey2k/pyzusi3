7c759606fe81c7179f24822a5e60f869c7a70f3d
Markus Ullmann <mail@markus-ullmann.de>
2022-10-09 01:41:50 +0200

    Move client part from demo into its own class

--- demo.py
+++ demo.py
@@ -4,221 +4,52 @@ import os.path
 import sys
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-from pyzusi3.messagecoders import MessageDecoder, encode_obj
 from pyzusi3 import messages
-from pyzusi3.nodes import AsyncStreamDecoder
+from pyzusi3.client import ZusiClient, suppress_none_values
 
 ZUSI_IP = "127.0.0.1"
 ZUSI_PORT = "1436"
-LOG_MSG_UPDATES = False
 
+WAIT_MAX = 3000 # keep running for 5 minutes
+
+logging.basicConfig(level=logging.WARNING)
 log = logging.getLogger("ZusiDemo")
-logging.basicConfig(level=logging.INFO)
-logging.getLogger("pyzusi3.node").setLevel(logging.WARNING)
-logging.getLogger("pyzusi3.streamdata").setLevel(logging.WARNING)
-logging.getLogger("pyzusi3.messagecoders.MessageDecoder").setLevel(logging.WARNING)
-
-tasks = []
-
-
-def without_null_keys(dictionary):
-    cleaned_collection = {}
-    for key, value in dictionary.items():
-        if value is not None:
-            cleaned_collection[key] = value
-
-    return cleaned_collection
+log.setLevel(logging.INFO)
 
 
 def handle_exception(loop, context):
+    """Dummy handler to log errors in demo to console"""
     msg = context.get("exception", context["message"])
     log.error(f"Caught exception: {msg}")
 
 
-async def decode_bytes(stream_bytes):
-    decoder = AsyncStreamDecoder()
-    decoded_tree = decoder.decode(stream_bytes)
-    nodes = []
-    async for node in decoded_tree:
-        messagedecoder = MessageDecoder()
-        yield messagedecoder.parse(node)      
-
-
-async def zusi_writer(writer, to_zusi_queue):
-    while True:
-        msg = await to_zusi_queue.get()
-        log.debug("Sending msg to Zusi: %s %s" % (msg.__class__.__name__, str(without_null_keys(msg._asdict()))))
-        writer.write(encode_obj(msg).encode())
-        await asyncio.sleep(0.1)
-
-
-async def zusi_reader(reader, from_zusi_queue):
-    msg_reader = decode_bytes(reader)
-
-    while True:
-        basemessage, submessages = await msg_reader.__anext__()
-        if basemessage is not None:
-            log.debug("Got basemessage from Zusi: %s %s" % (basemessage.__class__.__name__, str(without_null_keys(basemessage._asdict()))))
-            from_zusi_queue.put_nowait(basemessage)
-        for message in submessages:
-            log.debug("Got submessage from Zusi: %s %s" % (message.__class__.__name__, str(without_null_keys(message._asdict()))))
-            from_zusi_queue.put_nowait(message)
-        await asyncio.sleep(0.1)
-
-
-async def update_local_state(local_state, update_event, from_zusi_queue):
-    log.info("Awaiting new states")
-    while True:
-        msg = await from_zusi_queue.get()
-        log.debug("Got new data for %s" % str(type(msg)))
-        if msg is None:
-            continue
-
-        log.debug("Checking for known state")
-        msgtype = type(msg)
-        if msgtype not in local_state:
-            log.debug("Just saving whole state, was unknown")
-            local_state[msgtype] = msg
-            update_event.set()
-            continue
-
-        log.debug("Checking for state increment updates")
-        updated_keys = without_null_keys(msg._asdict())
-        if not updated_keys:
-            log.debug("None found, skipping")
-            continue
-        known_msg_state = local_state[msgtype]
-        unchanged_keys = []
-        for key in updated_keys.keys():
-            if getattr(known_msg_state, key) == updated_keys[key]:
-                unchanged_keys.append(key)
-        for key in unchanged_keys:
-            del updated_keys[key]
-        if not updated_keys:
-            log.debug("Just same content, skipping")
-            continue
-
-        log.debug("Updated keys: %s" % updated_keys)
-        local_state[msgtype] = known_msg_state._replace(**updated_keys)
-        update_event.set()
-
-        if not LOG_MSG_UPDATES:
-            continue
-
-        # normal messages.FAHRPULT_ANZEIGEN states
-        data = str(without_null_keys(local_state[msgtype]._asdict()))
-        if isinstance(msg, messages.DATA_FTD):
-            log.warning("Updated general train data: %s" % data)
-        # messages.FAHRPULT_ANZEIGEN.STATUS_NOTBREMSSYSTEM
-        elif isinstance(msg, messages.STATUS_NOTBREMSSYSTEM):
-            log.warning("Updated state for emer brakes: %s" % data)
-        # messages.FAHRPULT_ANZEIGEN.STATUS_SIFA
-        elif isinstance(msg, messages.STATUS_SIFA):
-            log.warning("Updated state for Sifa: %s" % data)
-        # messages.FAHRPULT_ANZEIGEN.STATUS_ZUGBEEINFLUSSUNG
-        elif isinstance(msg, messages.STATUS_ZUGBEEINFLUSSUNG_GRUND):
-            log.warning("Updated state for ZB basic: %s" % data)
-        elif isinstance(msg, messages.STATUS_INDUSI_EINSTELLUNGEN):
-            log.warning("Updated state for ZB settings: %s" % data)
-        elif isinstance(msg, messages.STATUS_INDUSI_BETRIEBSDATEN):
-            log.warning("Updated state for ZB state: %s" % data)
-        elif isinstance(msg, messages.STATUS_ETCS_EINSTELLUNGEN):
-            log.warning("Updated state for ETCS settings: %s" % data)
-        elif isinstance(msg, messages.STATUS_ETCS_BETRIEBSDATEN):
-            log.warning("Updated state for ETCS state: %s" % data)
-        elif isinstance(msg, messages.STATUS_ZUB_EINSTELLUNGEN):
-            log.warning("Updated state for ZUB settings: %s" % data)
-        elif isinstance(msg, messages.STATUS_ZUB_BETRIEBSDATEN):
-            log.warning("Updated state for ZUB state: %s" % data)
-        elif isinstance(msg, messages.STATUS_ZBS_EINSTELLUNGEN):
-            log.warning("Updated state for ZBS settings: %s" % data)
-        elif isinstance(msg, messages.STATUS_ZBS_BETRIEBSDATEN):
-            log.warning("Updated state for ZBS state: %s" % data)
-        # messages.FAHRPULT_ANZEIGEN.STATUS_TUEREN
-        elif isinstance(msg, messages.STATUS_TUEREN):
-            log.warning("Updated state for doors: %s" % data)
-        # messages.FAHRPULT_ANZEIGEN.STATUS_FAHRZEUG
-        elif isinstance(msg, messages.STATUS_FAHRZEUG):
-            log.warning("Updated state for vehicle: %s" % data)
-        # messages.FAHRPULT_ANZEIGEN.STATUS_ZUGVERBAND
-        elif isinstance(msg, messages.STATUS_ZUGVERBAND):
-            log.warning("Updated state for train: %s" % data)
-        # messages.FAHRPULT_ANZEIGEN.STATUS_WEICHEN
-        elif isinstance(msg, messages.STATUS_WEICHEN):
-            log.warning("Updated state for switch: %s" % data)
-        # messages.FAHRPULT_ANZEIGEN.STATUS_LM_ZUSIPISPLAY
-        elif isinstance(msg, messages.STATUS_LM_ZUSIDISPLAY):
-            log.warning("Updated state for zusidisplay buttons: %s" % data)
-        # messages.FAHRPULT_ANZEIGEN.STATUS_ZUGFAHRDATEN
-        elif isinstance(msg, messages.STATUS_ZUGFAHRDATEN):
-            log.warning("Updated state for train wagon data: %s" % data)
-        elif isinstance(msg, messages.DATA_PROG):
-            log.warning("Updated simulation data: %s" % data)
-        # messages.bedienung = True
-        elif isinstance(msg, messages.DATA_OPERATION):
-            log.warning("Updated input data: %s" % data)
-        else:
-            log.error("Unsorted message: type: %s content: %s" % (type(msg), data))
-
-
-async def zusi_user_interact(local_state, update_event, to_zusi_queue):
-    wait_max = 3000
-    wait_cur = 0
+async def zusi_user_interact(client: ZusiClient):
+    """Simulation for some random user input to be sent to Zusi"""
 
     zuord = messages.INPUT_TASTATURZUORDNUNG
     komm = messages.INPUT_TASTATURKOMMANDO
     aktion = messages.INPUT_TASTATURAKTION
 
     simulated_interactions = {
-        100: messages.INPUT(
-            tastatur_zuordnung=zuord.FAHRSCHALTER,
-            tastatur_kommando=komm.Unbestimmt,
-            tastatur_aktion=aktion.Absolut,
-            tastatur_schalterposition=5,
-            ),
-        109: messages.INPUT(
-            tastatur_zuordnung=zuord.SIFA,
-            tastatur_kommando=komm.Unbestimmt,
-            tastatur_aktion=aktion.Down,
-            tastatur_schalterposition=0,
-            ),
-        110: messages.INPUT(
-            tastatur_zuordnung=zuord.SIFA,
-            tastatur_kommando=komm.Unbestimmt,
-            tastatur_aktion=aktion.Up,
-            tastatur_schalterposition=0,
-            ),
-        150: messages.INPUT(
-            tastatur_zuordnung=zuord.FAHRSCHALTER,
-            tastatur_kommando=komm.Unbestimmt,
-            tastatur_aktion=aktion.Absolut,
-            tastatur_schalterposition=0,
-            ),
-        160: messages.INPUT(
-            tastatur_zuordnung=zuord.SIFA,
-            tastatur_kommando=komm.Unbestimmt,
-            tastatur_aktion=aktion.Down,
-            tastatur_schalterposition=0,
-            ),
-        169: messages.INPUT(
-            tastatur_zuordnung=zuord.SIFA,
-            tastatur_kommando=komm.Unbestimmt,
-            tastatur_aktion=aktion.Up,
-            tastatur_schalterposition=0,
-            )
+        100: messages.INPUT(zuord.FAHRSCHALTER, komm.Unbestimmt, aktion.Absolut, tastatur_schalterposition=5),
+        109: messages.INPUT(zuord.SIFA, komm.Unbestimmt, aktion.Down, tastatur_schalterposition=0),
+        110: messages.INPUT(zuord.SIFA, komm.Unbestimmt, aktion.Up, tastatur_schalterposition=0),
+        150: messages.INPUT(zuord.FAHRSCHALTER, komm.Unbestimmt, aktion.Absolut, tastatur_schalterposition=0),
+        160: messages.INPUT(zuord.SIFA, komm.Unbestimmt, aktion.Down, tastatur_schalterposition=0),
+        165: messages.INPUT(zuord.SIFA, komm.Unbestimmt, aktion.Up, tastatur_schalterposition=0)
     }
-    simulated_interactions = {}
 
     known_states = {}
+    wait_cur = 0
     while True:
-        # do not wait here, we do that below to have ~5 mins runtime
-        if update_event.is_set():
-            update_event.clear()
-            for message in local_state:
-                current_state = local_state[message]._asdict()
+        # handle incremental updates from Zusi to print only changed state to console
+        if client.local_state_changed.is_set():
+            client.local_state_changed.clear()
+            for message, state in client.local_state.items():
+                current_state = state._asdict()
                 if message not in known_states:
                     known_states[message] = current_state
-                    log.info("%s state: %s" % (message.__name__, without_null_keys(current_state)))
+                    log.info("%s state: %s" % (message.__name__, suppress_none_values(current_state)))
                     continue
                 try:
                     updates = set(current_state.items()) - set(known_states[message].items())
@@ -234,95 +65,49 @@ async def zusi_user_interact(local_state, update_event, to_zusi_queue):
         if wait_cur in simulated_interactions:
             msg = simulated_interactions[wait_cur]
             log.info("Sending new action %s: %s" % (msg.__class__.__name__, str(without_null_keys(msg._asdict()))))
-            await to_zusi_queue.put(msg)
-        if wait_cur > wait_max:
+            client.send_input(msg)
+        if wait_cur > WAIT_MAX:
+            log.info("Time is up, leaving")
             break
 
-async def client_demo(ip, port):
-    log.info("Connecting to Zusi3")
-    tcpreader, tcpwriter = await asyncio.open_connection(
-        ip, port)
-
-    log.info("Starting reader and writer task")
-    from_zusi_queue = asyncio.Queue()
-    to_zusi_queue = asyncio.Queue()
-    
-    reader_task = asyncio.create_task(zusi_reader(tcpreader, from_zusi_queue))
-    tasks.append(reader_task)
-    writer_task = asyncio.create_task(zusi_writer(tcpwriter, to_zusi_queue))
-    tasks.append(writer_task)
-
-    log.info("Sending HELLO message")
-    msg = messages.HELLO(2, messages.ClientTyp.FAHRPULT, "Schlumpfpult", "1.0")
-    to_zusi_queue.put_nowait(msg)
-
-    log.info("Waiting for response")
-    msg = await from_zusi_queue.get()
-    if not (isinstance(msg, messages.ACK_HELLO) and msg.status == 0):
-        log.error("Zusi did not report success for HELLO")
-        return
-
-    log.info("Request some regular status updates from Zusi")
-    msg = messages.NEEDED_DATA(
-        anzeigen=[
+tasks = []
+async def main(ip, port):
+    client = ZusiClient(ip, port, "pyzusi3 demo", "1.0")
+    client.request_status(displays=[
             messages.FAHRPULT_ANZEIGEN.GESCHWINDIGKEIT,
             messages.FAHRPULT_ANZEIGEN.STATUS_SIFA,
             messages.FAHRPULT_ANZEIGEN.STATUS_ZUGBEEINFLUSSUNG
         ],
-        bedienung=True,
-        programmdaten=[
+        control=True,
+        programdata=[
             messages.PROGRAMMDATEN.ZUGDATEI,
             messages.PROGRAMMDATEN.ZUGNUMMER,
             messages.PROGRAMMDATEN.LADEPAUSE,
         ]
     )
-    await to_zusi_queue.put(msg)
 
-    log.info("Awaiting status request ok")
-    msg = await from_zusi_queue.get()
-    if not (isinstance(msg, messages.ACK_NEEDED_DATA) and msg.status == 0):
-        log.error("Zusi did not report success for NEEDED_DATA")
-        return
-
-    log.info("Creating task to update local state when Zusi reports data")
-    local_state = {}
-    update_event = asyncio.Event()
-    update_task = asyncio.create_task(update_local_state(local_state, update_event, from_zusi_queue))
-    tasks.append(update_task)
-    log.info("Creating task to simulate interaction with Zusi")
-    interact_task = asyncio.create_task(zusi_user_interact(local_state, update_event, to_zusi_queue))
-    tasks.append(interact_task)
-
-    # wait until interact finishes, either getting the stop element or CTRL-C
-    try:
-        await asyncio.wait([interact_task])
-    except KeyboardInterrupt:
-        pass
-
-    # cancel all other tasks
-    reader_task.cancel()
-    writer_task.cancel()
-    update_task.cancel()
-    
-    tcpwriter.close()
-
-    log.info("All done")
-
-async def main(ip, port):
-    main_task = asyncio.create_task(client_demo(ZUSI_IP, ZUSI_PORT))
+    main_task = asyncio.create_task(client.connect())
     tasks.append(main_task)
 
+    interact_task = asyncio.create_task(zusi_user_interact(client))
+    tasks.append(interact_task)
+
     while True:
+        all_tasks_done = True
         for task in tasks:
-            #print(task)
             if task.done():
                 exc = task.exception()
                 if exc:
                     log.exception(exc, exc_info=exc)
                     pass
+            else:
+                all_tasks_done = False
+        if all_tasks_done:
+            break
         await asyncio.sleep(1)
 
-run_loop = asyncio.new_event_loop()
-#run_loop.set_exception_handler(handle_exception)
-run_loop.create_task(main(ZUSI_IP, ZUSI_PORT))
-run_loop.run_forever()
+if __name__ == "__main__":
+    run_loop = asyncio.new_event_loop()
+    run_loop.set_exception_handler(handle_exception)
+    run_loop.create_task(main(ZUSI_IP, ZUSI_PORT))
+    run_loop.run_forever()
---
+++ pyzusi3/client.py
@@ -0,0 +1,242 @@
+import asyncio
+import logging
+
+from pyzusi3.nodes import AsyncStreamDecoder
+from pyzusi3 import messages
+from pyzusi3.messagecoders import MessageDecoder, encode_obj
+
+reader_log = logging.getLogger("pyzusi3.client.reader")
+writer_log = logging.getLogger("pyzusi3.client.writer")
+updater_log = logging.getLogger("pyzusi3.client.updater")
+client_log = logging.getLogger("pyzusi3.client")
+
+
+# Debug function
+LOG_MSG_UPDATES = False
+
+
+def suppress_none_values(dictionary):
+    """Helper to just return items from dictionary where the value is not None"""
+
+    cleaned_collection = {}
+    for key, value in dictionary.items():
+        if value is not None:
+            cleaned_collection[key] = value
+
+    return cleaned_collection
+
+
+class ZusiClient:
+    def __init__(self, ip, port, clientname="pyZusi3 Fahrpult", clientversion="1.0"):
+        self.ip = ip
+        self.port = port
+        self.clientname = clientname
+        self.clientversion = clientversion
+
+        self.task_registry = {}
+        
+        self.send_messagequeue = asyncio.Queue()
+        self.receive_messagequeue = asyncio.Queue()
+        self.local_state = {}
+        self.message_update_events = {}
+        self.local_state_changed = asyncio.Event()
+
+        self.connected = False
+        self.requested_status = None
+
+    async def _decode_bytes(self, stream_bytes):
+        """Generator to get decoded messages from a stream"""
+
+        decoder = AsyncStreamDecoder()
+        decoded_tree = decoder.decode(stream_bytes)
+        nodes = []
+        async for node in decoded_tree:
+            messagedecoder = MessageDecoder()
+            yield messagedecoder.parse(node)      
+
+    async def _zusi_writer(self, write_stream):
+        """Queue worker to send messages to Zusi on the stream"""
+
+        while True:
+            msg = await self.send_messagequeue.get()
+            writer_log.debug("Sending msg to Zusi: %s %s" % (msg.__class__.__name__, str(suppress_none_values(msg._asdict()))))
+            write_stream.write(encode_obj(msg).encode())
+            await asyncio.sleep(0.1)
+
+    async def _zusi_reader(self, reader):
+        """Queue worker to process received messages from Zusi on given stream"""
+
+        msg_reader = self._decode_bytes(reader)
+
+        while True:
+            basemessage, submessages = await msg_reader.__anext__()
+            if basemessage is not None:
+                reader_log.debug("Got basemessage from Zusi: %s %s" % (basemessage.__class__.__name__, str(suppress_none_values(basemessage._asdict()))))
+                self.receive_messagequeue.put_nowait(basemessage)
+            for message in submessages:
+                reader_log.debug("Got submessage from Zusi: %s %s" % (message.__class__.__name__, str(suppress_none_values(message._asdict()))))
+                self.receive_messagequeue.put_nowait(message)
+            await asyncio.sleep(0.1)
+
+    async def _update_local_state(self):
+        updater_log.info("Awaiting new states")
+        while True:
+            msg = await self.receive_messagequeue.get()
+            updater_log.debug("Got new data for %s" % str(type(msg)))
+            if msg is None:
+                continue
+
+            updater_log.debug("Checking for known state")
+            msgtype = type(msg)
+            if msgtype not in self.local_state:
+                updater_log.debug("Just saving whole state, was unknown")
+                self.local_state[msgtype] = msg
+                self.message_update_events[msgtype] = asyncio.Event()
+                self.local_state_changed.set()
+                continue
+
+            updater_log.debug("Checking for state increment updates")
+            updated_keys = suppress_none_values(msg._asdict())
+            if not updated_keys:
+                updater_log.debug("None found, skipping")
+                continue
+            known_msg_state = self.local_state[msgtype]
+            unchanged_keys = []
+            for key in updated_keys.keys():
+                if getattr(known_msg_state, key) == updated_keys[key]:
+                    unchanged_keys.append(key)
+            for key in unchanged_keys:
+                del updated_keys[key]
+            if not updated_keys:
+                updater_log.debug("Just same content, skipping")
+                continue
+
+            updater_log.debug("Updated keys: %s" % updated_keys)
+            self.local_state[msgtype] = known_msg_state._replace(**updated_keys)
+            self.message_update_events[msgtype].set()
+            self.local_state_changed.set()
+
+            if not LOG_MSG_UPDATES:
+                continue
+
+            # normal messages.FAHRPULT_ANZEIGEN states
+            data = str(suppress_none_values(self.local_state[msgtype]._asdict()))
+            if isinstance(msg, messages.DATA_FTD):
+                updater_log.warning("Updated general train data: %s" % data)
+            # messages.FAHRPULT_ANZEIGEN.STATUS_NOTBREMSSYSTEM
+            elif isinstance(msg, messages.STATUS_NOTBREMSSYSTEM):
+                updater_log.warning("Updated state for emer brakes: %s" % data)
+            # messages.FAHRPULT_ANZEIGEN.STATUS_SIFA
+            elif isinstance(msg, messages.STATUS_SIFA):
+                updater_log.warning("Updated state for Sifa: %s" % data)
+            # messages.FAHRPULT_ANZEIGEN.STATUS_ZUGBEEINFLUSSUNG
+            elif isinstance(msg, messages.STATUS_ZUGBEEINFLUSSUNG_GRUND):
+                updater_log.warning("Updated state for ZB basic: %s" % data)
+            elif isinstance(msg, messages.STATUS_INDUSI_EINSTELLUNGEN):
+                updater_log.warning("Updated state for ZB settings: %s" % data)
+            elif isinstance(msg, messages.STATUS_INDUSI_BETRIEBSDATEN):
+                updater_log.warning("Updated state for ZB state: %s" % data)
+            elif isinstance(msg, messages.STATUS_ETCS_EINSTELLUNGEN):
+                updater_log.warning("Updated state for ETCS settings: %s" % data)
+            elif isinstance(msg, messages.STATUS_ETCS_BETRIEBSDATEN):
+                updater_log.warning("Updated state for ETCS state: %s" % data)
+            elif isinstance(msg, messages.STATUS_ZUB_EINSTELLUNGEN):
+                updater_log.warning("Updated state for ZUB settings: %s" % data)
+            elif isinstance(msg, messages.STATUS_ZUB_BETRIEBSDATEN):
+                updater_log.warning("Updated state for ZUB state: %s" % data)
+            elif isinstance(msg, messages.STATUS_ZBS_EINSTELLUNGEN):
+                updater_log.warning("Updated state for ZBS settings: %s" % data)
+            elif isinstance(msg, messages.STATUS_ZBS_BETRIEBSDATEN):
+                updater_log.warning("Updated state for ZBS state: %s" % data)
+            # messages.FAHRPULT_ANZEIGEN.STATUS_TUEREN
+            elif isinstance(msg, messages.STATUS_TUEREN):
+                updater_log.warning("Updated state for doors: %s" % data)
+            # messages.FAHRPULT_ANZEIGEN.STATUS_FAHRZEUG
+            elif isinstance(msg, messages.STATUS_FAHRZEUG):
+                updater_log.warning("Updated state for vehicle: %s" % data)
+            # messages.FAHRPULT_ANZEIGEN.STATUS_ZUGVERBAND
+            elif isinstance(msg, messages.STATUS_ZUGVERBAND):
+                updater_log.warning("Updated state for train: %s" % data)
+            # messages.FAHRPULT_ANZEIGEN.STATUS_WEICHEN
+            elif isinstance(msg, messages.STATUS_WEICHEN):
+                updater_log.warning("Updated state for switch: %s" % data)
+            # messages.FAHRPULT_ANZEIGEN.STATUS_LM_ZUSIPISPLAY
+            elif isinstance(msg, messages.STATUS_LM_ZUSIDISPLAY):
+                updater_log.warning("Updated state for zusidisplay buttons: %s" % data)
+            # messages.FAHRPULT_ANZEIGEN.STATUS_ZUGFAHRDATEN
+            elif isinstance(msg, messages.STATUS_ZUGFAHRDATEN):
+                updater_log.warning("Updated state for train wagon data: %s" % data)
+            elif isinstance(msg, messages.DATA_PROG):
+                updater_log.warning("Updated simulation data: %s" % data)
+            # messages.bedienung = True
+            elif isinstance(msg, messages.DATA_OPERATION):
+                updater_log.warning("Updated input data: %s" % data)
+            else:
+                updater_log.error("Unsorted message: type: %s content: %s" % (type(msg), data))
+
+    async def request_status(self, displays=[], control=False, programdata=[]):
+        self.requested_status = messages.NEEDED_DATA(
+            anzeigen=displays,
+            bedienung=control,
+            programmdaten=programdata
+        )
+        if self.connected and 'updater' in self.task_registry:
+            self.send_messagequeue.put_nowait(self.requested_status)
+
+    async def send_input(self, input):
+        if not isinstance(input, messages.INPUT):
+            raise ValueError("Input needs to be in message INPUT format")
+        
+        if not self.connected:
+            raise ConnectionError("Not connected to Zusi")
+        self.send_messagequeue.put_nowait(input)
+
+    async def connect(self):
+        client_log.info("Connecting to Zusi3")
+        tcpreader, tcpwriter = await asyncio.open_connection(self.ip, self.port)
+        self.connected = True
+
+        client_log.info("Starting reader and writer task")
+        reader_task = asyncio.create_task(self._zusi_reader(tcpreader))
+        self.task_registry['reader'] = reader_task
+        writer_task = asyncio.create_task(self._zusi_writer(tcpwriter))
+        self.task_registry['writer'] = writer_task
+
+        client_log.info("Sending HELLO message")
+        msg = messages.HELLO(2, messages.ClientTyp.FAHRPULT, self.clientname, self.clientversion)
+        self.send_messagequeue.put_nowait(msg)
+
+        client_log.info("Waiting for response")
+        msg = await self.receive_messagequeue.get()
+        if not (isinstance(msg, messages.ACK_HELLO) and msg.status == 0):
+            client_log.error("Zusi did not report success for HELLO")
+            return
+
+        client_log.info("Waiting for user status to request content")
+        while self.requested_status is None:
+            await asyncio.sleep(0.1)
+        self.send_messagequeue.put_nowait(self.requested_status)
+
+        client_log.info("Awaiting status response ok from Zusi")
+        msg = await self.receive_messagequeue.get()
+        if not (isinstance(msg, messages.ACK_NEEDED_DATA) and msg.status == 0):
+            client_log.error("Zusi did not report success for NEEDED_DATA")
+            return
+
+        client_log.info("Creating task to update local state when Zusi reports data")
+        update_task = asyncio.create_task(self._update_local_state())
+        self.task_registry['updater'] = update_task
+
+        # wait for communication to finish
+        await asyncio.wait([reader_task])
+
+        # cancel all other tasks and close connection
+        writer_task.cancel()
+        update_task.cancel()
+
+        tcpreader.close()    
+        tcpwriter.close()
+
+        self.connected = False
+
+        client_log.info("Connection closed")
